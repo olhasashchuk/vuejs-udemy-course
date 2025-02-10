fb5bf53c05e9ab9f0a7a3edca87d8870b22f7a4d
Olha Sashchuk <olha.sashchuk@gmail.com>
2025-02-10 14:06:16 +0100

    implemented components communication assignment

---
+++ components-communication-assignment/src/components/ActiveUser.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <h3>Age: {{ age }}</h3>
+    <button @click="$emit('call-edit-user', { id, name, age })">
+      Edit User
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ components-communication-assignment/src/components/UserData.vue
@@ -0,0 +1,59 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" placeholder="Name" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Age</label>
+      <input type="text" placeholder="Age" v-model="enteredAge" />
+    </div>
+    <div>
+      <button type="submit">{{ isEditing ? "Edit" : "Add" }} User</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    editingUser: Object, // Принимаем данные пользователя для редактирования
+  },
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+      isEditing: false,
+      userId: null,
+    };
+  },
+  watch: {
+    editingUser(newUser) {
+      if (newUser) {
+        this.enteredName = newUser.name;
+        this.enteredAge = newUser.age;
+        this.userId = newUser.id;
+        this.isEditing = true;
+      }
+    },
+  },
+  emits: ["add-user", "edit-user"],
+  methods: {
+    submitData() {
+      if (this.isEditing) {
+        this.$emit("edit-user", {
+          id: this.userId,
+          name: this.enteredName,
+          age: this.enteredAge,
+        });
+        this.isEditing = false;
+        this.userId = null;
+      } else {
+        this.$emit("add-user", this.enteredName, this.enteredAge);
+      }
+      this.enteredName = "";
+      this.enteredAge = "";
+    },
+  },
+};
+</script>
