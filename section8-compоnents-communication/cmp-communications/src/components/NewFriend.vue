4e309b058015145a67b9466ecca3b09c2fa8b33f
Olha Sashchuk <olha.sashchuk@gmail.com>
2025-02-10 11:56:36 +0100

    implemented component communication

---
+++ section8-compоnents-communication/cmp-communications/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" placeholder="Name" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="text" placeholder="Phone" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="text" placeholder="Email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <button type="submit">Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enteredName: '',
+      enteredPhone: '',
+      enteredEmail: '',
+    }
+  },
+  emits: ['add-contact'],
+  methods: {
+    submitData() {
+      this.$emit(
+        'add-contact', 
+        this.enteredName, 
+        this.enteredPhone, 
+        this.enteredEmail
+      )
+    },
+  },
+}
+</script>
