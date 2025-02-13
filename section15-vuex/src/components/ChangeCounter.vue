4482e1567139410eda8c2d4ebfe5e3289b085021
Olha Sashchuk <olha.sashchuk@gmail.com>
2025-02-13 19:32:08 +0100

    implemented vuex

---
+++ section15-vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div> 
+    <button @click="inc">Add2</button>
+  <button @click="increase({value: 11})">Add11</button>
+
+  </div>
+  
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // addOne() {
+    //   this.$store.dispatch('increment');
+    // },
+    //...mapActions(['increment', 'increase'])
+    ...mapActions('counter', {
+      inc: 'increment',
+      increase: 'increase'
+    })
+  }
+}
+</script>
